f6cb5d85dd3d46d79bcf37a772ba1e50443f35b4
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-10-20 15:05:18 -0300

    Updated code for postList

--- postList.sql
+++ postList.sql
@@ -1,1 +1,9 @@
-/*hi*/
+delimiter //
+drop procedure if exists "postList"//
+
+create procedure postList(in usID varchar(16), title varchar(16))
+begin
+	insert into ToDoLists (UserID, Title) values
+		(usID, title);
+end//
+delimiter ;
