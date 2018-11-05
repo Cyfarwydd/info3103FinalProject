15fc42bd9bb8f311ada0242bcccb3a2175f7d088
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-11-05 12:56:46 -0400

    Changed strings so they could be 64 chars instead of 16

--- Tables_ToDo.sql
+++ Tables_ToDo.sql
@@ -1,13 +1,13 @@
 create table if not exists Users
 (
-	UserID	VARCHAR(16) not null primary key
+	UserID	VARCHAR(64) not null primary key
 );
 
 create table if not exists ToDoLists
 (
 	ListID	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	Title VARCHAR(16),
-	UserID VARCHAR(16) NOT NULL,
+	Title VARCHAR(64),
+	UserID VARCHAR(64) NOT NULL,
 	FOREIGN KEY(UserID)
 		REFERENCES Users(UserID) ON DELETE CASCADE
 );
@@ -16,7 +16,7 @@ create table if not exists Tasks
 (
 	TaskID int not null auto_increment primary key,
 	ListID int not null,
-	Task VARCHAR(50),
+	Task VARCHAR(64),
 	Complete boolean,
 	FOREIGN KEY(ListID)
 		REFERENCES ToDoLists(ListID) ON DELETE CASCADE
