a100bfa18662f8bd93687eca6363378fab6362f9
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-11-05 15:00:18 -0400

    Fixed again

--- finishTask.sql
+++ finishTask.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE finishTask(
 	lID int
 )
 	BEGIN
-	if (select ListID from ToDoList where (UserID = usID AND ListID = lID)) then
+	if (select ListID from ToDoLists where (UserID = usID AND ListID = lID)) then
 		UPDATE Tasks SET Complete = true where TaskID = tID;
 		end if;
 	END //
