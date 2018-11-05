05be8c90d4441471feb9052dc9244bb539e26ade
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-11-05 15:16:02 -0400

    Fixed Varchar lengths

--- postList.sql
+++ postList.sql
@@ -1,7 +1,7 @@
 delimiter //
-drop procedure if exists "postList"//
+drop procedure if exists postList//
 
-create procedure postList(in usID varchar(16), title varchar(16))
+create procedure postList(in usID varchar(64), title varchar(64))
 begin
 	insert into ToDoLists (UserID, Title) values
 		(usID, title);
