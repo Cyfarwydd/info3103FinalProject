84ba13d0739f5fca89e31c4db73a2352fd96beb2
ddoran1 <ddoran1@unb.ca>
2018-11-09 13:29:21 -0400

    Proc now only returns the userid

--- sql/getUserByName.sql
+++ sql/getUserByName.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS getUserByName//
 
 CREATE PROCEDURE getUserByName(IN name VARCHAR(64))
 BEGIN
-	select * from Users
+	select UserID from Users
 		where UserName = name;
 END//
 
