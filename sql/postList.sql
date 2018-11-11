e14ab6dc6f3b94adb4a8b185d9ed0f993fba6b6e
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-11-11 12:02:10 -0400

    Changed int of usID to varchar(64)

--- sql/postList.sql
+++ sql/postList.sql
@@ -1,7 +1,7 @@
 delimiter //
 drop procedure if exists postList//
 
-create procedure postList(in usID int, title varchar(64))
+create procedure postList(in usID varchar(64), title varchar(64))
 begin
 	insert into ToDoLists (UserID, Title) values
 		(usID, title);
