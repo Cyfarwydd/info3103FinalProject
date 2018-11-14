c3b0b8b017f8560263886cd80bcd85ab99764112
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-11-14 14:53:43 -0400

    Fixed postList o insert user id not user name

--- sql/postList.sql
+++ sql/postList.sql
@@ -3,7 +3,9 @@ drop procedure if exists postList//
 
 create procedure postList(in usID varchar(64), title varchar(64))
 begin
+	declare id int;
+	select UserID from Users where UserName = usID into id;
 	insert into ToDoLists (UserID, Title) values
-		(usID, title);
+		(id, title);
 end//
 delimiter ;
