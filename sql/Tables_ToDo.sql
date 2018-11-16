8753097eb242e16da0d07928867f5e92d5b1be23
ddoran1 <ddoran1@unb.ca>
2018-11-16 15:14:45 -0400

    added row description column to ToDoLists and made some corrections to Tasks

--- sql/Tables_ToDo.sql
+++ sql/Tables_ToDo.sql
@@ -9,6 +9,7 @@ create table ToDoLists
 (
 	ListID int unique not null AUTO_INCREMENT PRIMARY KEY,
 	Title VARCHAR(64),
+	Description VARCHAR(64),
 	UserID int NOT NULL,
 	FOREIGN KEY(UserID) REFERENCES Users(UserID) ON DELETE CASCADE
 );
@@ -18,7 +19,7 @@ create table Tasks
 	TaskID int not null auto_increment primary key,
 	ListID int not null,
 	Task VARCHAR(64),
-	Complete boolean,
+	Complete boolean DEFAULT 0,
 	FOREIGN KEY(ListID) REFERENCES ToDoLists(ListID) ON DELETE CASCADE
 );
 
