9e92c1c47d2c2e40f83ad19448d1d022103e9ec8
ddoran1 <ddoran1@unb.ca>
2018-11-16 15:15:30 -0400

    Working version created

--- sql/createTask.sql
+++ sql/createTask.sql
@@ -1,16 +1,11 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS createTask//
 
-CREATE PROCEDURE createTask(
-	in
-	usID int,
-	lID int,
-	description varchar(64)
-)
+CREATE PROCEDURE createTask(in usID int, in lID int, in taskIn varchar(64))
 BEGIN
-	INSERT INTO Tasks (Task, Complete, ListID) values ( description, false,
+	INSERT INTO Tasks (Task, ListID) values (taskIn,
 		(SELECT ListID FROM ToDoLists WHERE (UserID = usID AND ListID = lID))
 	);
-		
+
 END //
 DELIMITER ;
