2fa3b53899621f499541aaf42681bce8b635b298
ddoran1 <ddoran1@unb.ca>
2018-11-16 15:29:01 -0400

    Working version

--- sql/deleteList.sql
+++ sql/deleteList.sql
@@ -1,14 +1,13 @@
 DELIMITER //
-DROP PROCEDURE deleteList //
+DROP PROCEDURE if exists deleteList //
 
-CREATE PROCEDURE deleteList(
-	in lID int,
-	usID int
-)
-
+CREATE PROCEDURE deleteList(in lID int,usID int)
 BEGIN
 	DELETE FROM ToDoLists
 		WHERE (ListID = lID
 		AND UserID = usID);
+
+	DELETE FROM Tasks
+		WHERE ListID = lID;
 END//
 DELIMITER ;
